a7e31ee7e4e8b50f42a3c5313010fad932927ad5
Matheus1415 <115950084+Matheus1415@users.noreply.github.com>
2024-05-19 09:33:59 -0300

    codego

--- Desafio02/script.js
+++ Desafio02/script.js
@@ -1,28 +1,32 @@
 // script.js
 document.addEventListener('DOMContentLoaded', () => {
-    const itemInput = document.getElementById('itemInput');
-    const addItemBtn = document.getElementById('addItemBtn');
-    const shoppingList = document.getElementById('shoppingList');
+    //Pegue o item input
+    const itemInput = document.getElementById('');
+    //Pegue o item btn
+    const addItemBtn = document.getElementById('');
+    //Pegue o item shoppingList
+    const shoppingList = document.getElementById('');
 
-    let items = [];
+    let items = /*Diga que é um item de array vazio*/;
 
-    addItemBtn.addEventListener('click', addItem);
+    //adicionar evento click
+    addItemBtn.addEventListener('diga que é o evento click', /*coloque a função addItem*/);
 
     function addItem() {
-        const itemText = itemInput.value.trim();
+        const itemText = /*pegue o valor do input sem espaços*/;
         if (itemText) {
-            items.push(itemText);
-            itemInput.value = '';
-            renderItems();
+            items.push(/*Adicione o itemText*/);
+            itemInput.value = /*Diga que é uma string vazia*/;
+            //Chame  função renderItem a baixo do comentario
         }
     }
 
     function renderItems() {
-        shoppingList.innerHTML = '';
-        items.forEach(item => {
-            const li = document.createElement('li');
-            li.textContent = item;
-            shoppingList.appendChild(li);
+        shoppingList.innerHTML = /*Diga que é uma string vazia*/;
+        items.forEach(/*Adicione o parametro item*/ => {
+            const li = document.createElement(/*Pase o valor li em formato de string*/);
+            li.textContent = /*Atribua item*/;
+            shoppingList.appendChild(/*Adicione o li*/);
         });
     }
 });
